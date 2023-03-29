faa6f864fc0011df2a175937ca9fb5c35e2d8d43
jiasyuanchu <jiasyuanchu@gmail.com>
2023-03-29 14:10:33 +0800

    feat: password generator completed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-// app.js
-// include packages and define server related variables
+// Include packages and define server related variables
 const express = require('express')
 const exphbs = require('express-handlebars')
+const generatePassword = require('./generate_password')
 const app = express()
 const port = 3000
 
@@ -9,9 +9,18 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting body-parser
+app.use(express.urlencoded({ extended: true }))
+
 // setting routes
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const options = req.body
+  const password = generatePassword(options)
+  res.render('index', { password: password, options: options })
 })
 
 // starts the express server and listening for connections.
